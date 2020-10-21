7bc9d461fbd78f5da6ec240bace2061135e53817
bql20000 <bql20000@gmail.com>
2020-10-21 17:04:39 +0700

    implement user model

--- app/models/UserModel.py
+++ app/models/UserModel.py
@@ -2,6 +2,9 @@ from app.database import db
 
 
 class UserModel(db.Model):
+    """
+        Docs ...
+    """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
@@ -11,13 +14,14 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
-    def json(self):
-        return {'username': self.username}
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
-    def find_by_username(cls):
-        pass
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
-    def find_by_id(cls):
-        pass
+    def find_by_id(cls, idx):
+        return cls.query.filter_by(id=idx).first()
